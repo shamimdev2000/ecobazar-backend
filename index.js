eeb6a67bac62383c8c23fe2caeda79fd55a3fea8
Md Shamim <mdshamim.en@gmail.com>
2026-04-24 11:35:02 -1200

    git ignore add

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require("express")
 const app = express()
+
+app.get('/', (req,res)=>{
+   res.send("hello world")
+   
+})
+
+app.listen(5000, ()=>{
+    console.log("Server Running on port 5000");
+    
+})
